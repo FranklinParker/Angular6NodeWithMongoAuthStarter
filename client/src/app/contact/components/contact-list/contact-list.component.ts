fd6030937b6191a0dcbc5ee4353d73a05398dfd6
Franklin Parker <fparker@deliveryman.com>
2018-06-29 20:00:44 -0400

    contact list table

--- client/src/app/contact/components/contact-list/contact-list.component.ts
+++ client/src/app/contact/components/contact-list/contact-list.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit, Input, OnDestroy} from '@angular/core';
+import {Component, OnInit, Input, OnDestroy, ViewChild} from '@angular/core';
 import {Contact} from "../../model/contact";
 import {ContactService} from "../../service/contact.service";
 import {Subscription} from "rxjs/index";
+import { MatTableDataSource, MatPaginator} from "@angular/material";
 
 @Component({
   selector: 'app-contact-list',
@@ -12,6 +13,9 @@ export class ContactListComponent implements OnInit, OnDestroy {
 
   contactList: Contact[] = [];
   contactListSubs: Subscription;
+  dataSource = new MatTableDataSource<Contact>([]);
+  displayedColumns = ['firstName','lastName','email', 'phone'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private contactService: ContactService) {
   }
@@ -20,11 +24,21 @@ export class ContactListComponent implements OnInit, OnDestroy {
     this.contactListSubs = this.contactService.getContactListObservable()
       .subscribe((contactList: Contact[]) => {
         this.contactList = contactList;
+        this.dataSource.data = this.contactList;
       });
   }
 
   ngOnDestroy() {
     this.contactListSubs.unsubscribe();
+  }
+
+  edit(contact: Contact){
+    console.log('edit contact', contact);
+  }
+
+  rowClicked(contact:Contact){
+    console.log('row clicked contact', contact);
+
   }
 
 }
